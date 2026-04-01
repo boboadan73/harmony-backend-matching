bec1c5246490de38f14628c37ce2c778ea1b910a
lenadraz <lena.draz@campus.technion.ac.il>
2026-04-01 23:23:28 +0300

    fix conflict

--- harmony_backend/backend/index.js
+++ harmony_backend/backend/index.js
@@ -450,8 +450,8 @@ app.get("/test-db", async (req, res) => {
 // =====================================
 // START SERVER
 // =====================================
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
