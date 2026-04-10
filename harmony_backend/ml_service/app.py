cdbd045bd49f0c7d850f73c37d0625554581edc1
boboadan73 <boboadan73@gmail.com>
2026-04-10 21:47:49 +0300

    Update app.py

--- harmony_backend/ml_service/app.py
+++ harmony_backend/ml_service/app.py
@@ -1,27 +1,28 @@
 import os
 from fastapi import FastAPI
 from pydantic import BaseModel
-from sentence_transformers import SentenceTransformer
 from typing import List
 
 app = FastAPI()
 
 model = None
 
-@app.on_event("startup")
-def load_model():
-    global model
-    model = SentenceTransformer(
-        "rayanmahmoud/harmony_model",
-        token=os.getenv("HF_TOKEN")
-    )
-    print("Model loaded successfully")
-
-
 class TextRequest(BaseModel):
     texts: List[str]
 
+def get_model():
+    global model
+    if model is None:
+        from sentence_transformers import SentenceTransformer
+        model = SentenceTransformer(
+            "rayanmahmoud/harmony_model",
+            token=os.getenv("HF_TOKEN")
+        )
+        print("Model loaded successfully")
+    return model
+
 @app.post("/embed")
 def embed_texts(req: TextRequest):
-    embeddings = model.encode(req.texts, normalize_embeddings=True).tolist()
+    model_instance = get_model()
+    embeddings = model_instance.encode(req.texts, normalize_embeddings=True).tolist()
     return {"embeddings": embeddings}
