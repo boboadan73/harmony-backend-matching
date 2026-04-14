d30bac1c7666d4f82b3ce613397b1c1f8192b6be
rayan <rayanmahmoud@campus.technion.ac.il>
2026-04-14 14:43:53 +0300

    fix: add

--- harmony_backend/backend/index.js
+++ harmony_backend/backend/index.js
@@ -232,7 +232,7 @@ if (alreadyProcessing) {
 // );
 
 // Mark all participants in this event as processing
-const BatchSize = 40;
+const BatchSize = 30;
 
 for (let start = 0; start < resources.length; start += BatchSize) {
   const batch = resources.slice(start, start + BatchSize);
