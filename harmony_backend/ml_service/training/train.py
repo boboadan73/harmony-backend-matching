5859bb074a1d073ed014efa600f8ea21e29f9443
Mariam <mariam255.raw@gmail.com>
2026-07-05 16:02:57 +0300

    change train.py and index

--- harmony_backend/ml_service/training/train.py
+++ harmony_backend/ml_service/training/train.py
@@ -6,10 +6,6 @@ from torch.utils.data import DataLoader
 from dotenv import load_dotenv
 from huggingface_hub import login
 
-
-
-##########ADD TOKEN TO ENV##############################
-
 #train_data, val_data = build_dataset()
 
 event_id = None
@@ -21,19 +17,55 @@ print("Training for event_id:", event_id)
 
 train_data, val_data = build_dataset(event_id=event_id)
 
+
+old_train = sum(1 for ex in train_data if ex.source == "old")
+new_train = sum(1 for ex in train_data if ex.source == "new")
+
+new_positive_train = sum(1 for ex in train_data if ex.source == "new" and ex.label == 1.0)
+new_negative_train = sum(1 for ex in train_data if ex.source == "new" and ex.label == 0.0)
+
+old_positive_train = sum(1 for ex in train_data if ex.source == "old" and ex.label == 1.0)
+old_negative_train = sum(1 for ex in train_data if ex.source == "old" and ex.label == 0.0)
+
+print("Old train pairs:", old_train)
+print("New train pairs:", new_train)
+
+print("Old positive train pairs:", old_positive_train)
+print("Old negative train pairs:", old_negative_train)
+
+print("New positive train pairs:", new_positive_train)
+print("New negative train pairs:", new_negative_train)
+
 load_dotenv()
 
+# HF_TOKEN = os.getenv("HF_TOKEN")
+# HF_REPO_ID = os.getenv("HF_REPO_ID", "rayanmahmoud/harmony_model")
+
+# if HF_TOKEN:
+#     login(token=HF_TOKEN)
+
+# print("Using Hugging Face repo:", HF_REPO_ID)
+
+# # # model = SentenceTransformer("models/base_model")
+# # model = SentenceTransformer("rayanmahmoud/harmony_model")
+# model = SentenceTransformer(HF_REPO_ID)
+
 HF_TOKEN = os.getenv("HF_TOKEN")
-HF_REPO_ID = os.getenv("HF_REPO_ID", "rayanmahmoud/harmony_model")
+HF_BASE_REPO_ID = os.getenv("HF_BASE_REPO_ID", "rayanmahmoud/harmony_model")
+HF_WORKING_REPO_ID = os.getenv("HF_WORKING_REPO_ID", "rayanmahmoud/harmony_model_working")
 
 if HF_TOKEN:
     login(token=HF_TOKEN)
 
-print("Using Hugging Face repo:", HF_REPO_ID)
+print("Base model repo:", HF_BASE_REPO_ID)
+print("Working model repo:", HF_WORKING_REPO_ID)
 
-# # model = SentenceTransformer("models/base_model")
-# model = SentenceTransformer("rayanmahmoud/harmony_model")
-model = SentenceTransformer(HF_REPO_ID)
+try:
+    model = SentenceTransformer(HF_WORKING_REPO_ID)
+    print("Loaded existing working model")
+except Exception:
+    model = SentenceTransformer(HF_BASE_REPO_ID)
+    print("Working model not found, loaded base model")
 
 train_dataloader = DataLoader(
     train_data,
@@ -50,3 +82,12 @@ model.fit(
     output_path="models/harmony_model",
     show_progress_bar=True
 )
+
+print("Pushing updated model to Hugging Face...")
+
+model.push_to_hub(
+    repo_id=HF_WORKING_REPO_ID,
+    token=HF_TOKEN
+)
+
+print("Model pushed successfully to:", HF_WORKING_REPO_ID)
